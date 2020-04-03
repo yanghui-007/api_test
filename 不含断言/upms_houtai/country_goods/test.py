928cefbfae7d1ac14fbaaed10a4f437c8290b457
yanghui <yanghui1234561@126.com>
2020-04-03 18:25:45 +0800

    22

--- 不含断言/upms_houtai/country_goods/test.py
+++ 不含断言/upms_houtai/country_goods/test.py
@@ -68,8 +68,8 @@ class Test_countryproductlibrary(unittest.TestCase):
 
         res=get_request(self,url,headers,params)
 
-        print(res.status_code)#响应的HTTP状态码
-        print(res.reason)#响应的状态码含义
+        #print(res.status_code)#响应的HTTP状态码
+        #print(res.reason)#响应的状态码含义
 
         #print(res.text)
         #响应格式改为json格式
@@ -78,7 +78,7 @@ class Test_countryproductlibrary(unittest.TestCase):
         res_json=json.dumps(res_dict,indent=2,sort_keys=True,ensure_ascii=False)#响应格式改为json格式
         #print(ress)
         #print(res_dict)
-        print('---------------------------test003_/gameservProduct/countryProduct/page后台-商品管理-国家商品库#列表---------------------------')
+        #print('---------------------------test003_/gameservProduct/countryProduct/page后台-商品管理-国家商品库#列表---------------------------')
         #print(res_json)
 
         try:
@@ -116,7 +116,12 @@ class Test_countryproductlibrary(unittest.TestCase):
         #print(res_json)
 
         #self.assertIn('"code":0,"msg":"success"',res.text)
-      
+        try:
+            self.assertIn('"code":0,"msg":"success"',res.text)
+        except Exception as err:
+            # 正式运行前注释掉打印异常
+            print(err)
+            # print(ress)        
 
 
     #后台-商品管理-通用商品列表-关联sku列表分页
@@ -136,24 +141,24 @@ class Test_countryproductlibrary(unittest.TestCase):
 
         res=get_request(self,url,headers,params)
         #print(res.text)
-        print(res.status_code)#响应的HTTP状态码
-        print(res.reason)#响应的状态码含义
+        #print(res.status_code)#响应的HTTP状态码
+        #print(res.reason)#响应的状态码含义
 
         #响应格式为json格式
         ress=res.text
         res_dict=json.loads(ress)
         res_json=json.dumps(res_dict,indent=2,sort_keys=True,ensure_ascii=False)#响应格式为json格式
-        print('---------------------------test005_/gameservProduct/commonProduct/detailPage后台-商品管理-通用商品列表-关联sku列表分页---------------------------')
+        #print('---------------------------test005_/gameservProduct/commonProduct/detailPage后台-商品管理-通用商品列表-关联sku列表分页---------------------------')
         #print(res_json)
 
         #self.assertIn('"code":0,"msg":"success"',res.text)
 
-        # try:
-        #     self.assertIn('"code":0,"msg":"success"',res.text)
-        # except Exception as err:
-        #     # 正式运行前注释掉打印异常
-        #     print(err)
-        #     print(ress)         
+        try:
+            self.assertIn('"code":0,"msg":"success"',res.text)
+        except Exception as err:
+            # 正式运行前注释掉打印异常
+            print(err)
+            # print(ress)         
 
     #后台-商品管理-国家商品库#详情
     def test006_countryProductdetail(self):
@@ -172,18 +177,23 @@ class Test_countryproductlibrary(unittest.TestCase):
         res=get_request(self,url,headers,params)
         #print(res.text)
 
-        print(res.status_code)#相应的状态码
-        print(res.reason)#响应的状态码含义
+        #print(res.status_code)#相应的状态码
+        #print(res.reason)#响应的状态码含义
 
         #响应格式为json格式
         ress=res.text
         res_dict=json.loads(ress)
         res_json=json.dumps(res_dict,indent=2,sort_keys=True,ensure_ascii=False)#响应格式为json格式
-        print('---------------------------test006_/gameservProduct/countryProduct/detail后台-商品管理-国家商品库#详情---------------------------')
+        #print('---------------------------test006_/gameservProduct/countryProduct/detail后台-商品管理-国家商品库#详情---------------------------')
         #print(res_json)
 
         #self.assertIn('"code":0,"msg":"success"',res.text)
-
+        try:
+            self.assertIn('"code":0,"msg":"success"',res.text)
+        except Exception as err:
+            # 正式运行前注释掉打印异常
+            print(err)
+            # print(ress)  
 
     #后台-优惠券管理-列表
     def test007_gameservCouponpage(self):
@@ -202,18 +212,23 @@ class Test_countryproductlibrary(unittest.TestCase):
         res=get_request(self,url,headers,params)
         #print(res.text)
 
-        print(res.status_code)#相应的状态码
-        print(res.reason)#相应的状态码含义
+        #print(res.status_code)#相应的状态码
+        #print(res.reason)#相应的状态码含义
 
         #相应格式为json格式
         ress=res.text
         res_dict=json.loads(ress)
         res_json=json.dumps(res_dict,indent=2,sort_keys=True,ensure_ascii=False)#相应格式为json格式
-        print('---------------------------test007_/gameservSetting/gameservCoupon/page后台-优惠券管理-列表---------------------------')
+        #print('---------------------------test007_/gameservSetting/gameservCoupon/page后台-优惠券管理-列表---------------------------')
         #print(res_json)
 
         #self.assertIn('"code":0,"msg":"success"',res.text)
-        
+        try:
+            self.assertIn('"code":0,"msg":"success"',res.text)
+        except Exception as err:
+            # 正式运行前注释掉打印异常
+            print(err)
+            # print(ress)          
     #后台-订单管理-商品订单管理-page调整版
     def test008_OrderInfopage(self):
         url=urd+'/gameservOrder/gameservCardOrderInfo/page'
@@ -231,18 +246,23 @@ class Test_countryproductlibrary(unittest.TestCase):
         res=get_request(self,url,headers,params)
         #print(res.text)
 
-        print(res.status_code)#相应的状态码
-        print(res.reason)#相应的状态码含义
+        #print(res.status_code)#相应的状态码
+        #print(res.reason)#相应的状态码含义
 
         #相应格式为json格式
         ress=res.text
         res_dict=json.loads(ress)
         res_json=json.dumps(res_dict,indent=2,sort_keys=True,ensure_ascii=False)#相应格式为json格式
-        print('---------------------------test008_/gameservOrder/gameservCardOrderInfo/page后台-订单管理-商品订单管理-page调整版---------------------------')
+        #print('---------------------------test008_/gameservOrder/gameservCardOrderInfo/page后台-订单管理-商品订单管理-page调整版---------------------------')
         #print(res_json)
 
         #self.assertIn('"code":0,"msg":"success"',res.text)
-
+        try:
+            self.assertIn('"code":0,"msg":"success"',res.text)
+        except Exception as err:
+            # 正式运行前注释掉打印异常
+            print(err)
+            # print(ress)  
         #数据库校验
         # result=query_db("select order_no from gameserv_card_order_info_1 where id='1181835261673672706'")[0][0]
         # print(result)
@@ -265,18 +285,23 @@ class Test_countryproductlibrary(unittest.TestCase):
 
         #print(res.text)
 
-        print(res.status_code)#相应的状态码
-        print(res.reason)#相应的状态码含义
+        #print(res.status_code)#相应的状态码
+        #print(res.reason)#相应的状态码含义
 
         #相应格式为json格式
         ress=res.text
         res_dict=json.loads(ress)
         res_json=json.dumps(res_dict,indent=2,sort_keys=True,ensure_ascii=False)#相应格式为json格式
-        print('---------------------------test009_/gameservUser/rank/page后台-会员管理-会员等级管理-列表---------------------------')
+        #print('---------------------------test009_/gameservUser/rank/page后台-会员管理-会员等级管理-列表---------------------------')
         #print(res_json)
 
         #self.assertIn('"code":0,"msg":"success"',res.text)
-
+        try:
+            self.assertIn('"code":0,"msg":"success"',res.text)
+        except Exception as err:
+            # 正式运行前注释掉打印异常
+            print(err)
+            # print(ress)  
     # #获取余额代充报价
     # def test_010_getBalancePrice(self):
     #     url=urd+'/gameservProduct/productSetting/getBalancePrice'
